b7862f592834c1cb52a1c722eba33bdc971b7ad4
som1361 <s.zahedian@gmail.com>
2023-08-21 16:34:54 +1000

    fix android example build issue

--- example/android/app/src/main/java/com/iyaffle/launchreviewexample/MainActivity.java
+++ example/android/app/src/main/java/com/iyaffle/launchreviewexample/MainActivity.java
@@ -1,9 +1,6 @@
 package com.iyaffle.launchreviewexample;
+import io.flutter.embedding.android.FlutterActivity;
 
-import android.os.Bundle;
-
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
 
