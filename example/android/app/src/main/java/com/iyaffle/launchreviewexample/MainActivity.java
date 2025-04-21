737abf97f60f5a26449d2b17269ed8b38c05e093
somyzahedian <s.zahedian@gmail.com>
2025-04-22 00:35:31 +1000

    fix issues

--- example/android/app/src/main/java/com/iyaffle/launchreviewexample/MainActivity.java
+++ example/android/app/src/main/java/com/iyaffle/launchreviewexample/MainActivity.java
@@ -1,6 +1,6 @@
 package com.iyaffle.launchreviewexample;
-import io.flutter.embedding.android.FlutterActivity;
 
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
 
