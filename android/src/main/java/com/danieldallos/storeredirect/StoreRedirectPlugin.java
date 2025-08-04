9e7c361987c96373f5f4285d5fb6579a987682dc
somyzahedian <s.zahedian@gmail.com>
2025-08-04 11:35:33 +1000

    upgrade plugin

--- android/src/main/java/com/danieldallos/storeredirect/StoreRedirectPlugin.java
+++ android/src/main/java/com/danieldallos/storeredirect/StoreRedirectPlugin.java
@@ -8,7 +8,6 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import android.app.Activity;
 import android.content.Context;
@@ -22,14 +21,6 @@ public class StoreRedirectPlugin implements MethodCallHandler, FlutterPlugin, Ac
 
   private Activity activity;
   private MethodChannel methodChannel;
-
-  /**
-   * Plugin registration.
-   */
-  public static void registerWith(Registrar registrar) {
-    StoreRedirectPlugin instance = new StoreRedirectPlugin();
-    instance.onAttachedToEngine(registrar.messenger());
-  }
 
   @Override
   public void onAttachedToEngine(FlutterPluginBinding binding) {
